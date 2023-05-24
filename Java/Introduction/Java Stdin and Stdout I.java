3725c26fc0755b409027c3a447ddb702934886d5
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2023-05-24 22:54:01 +0530

    Update Java Stdin and Stdout I.java

--- Java/Introduction/Java Stdin and Stdout I.java
+++ Java/Introduction/Java Stdin and Stdout I.java
@@ -1,15 +1,17 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Solution {
-
     public static void main(String[] args) {
+        /* Read and save input */
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
-
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        
+        /* Print data */
         System.out.println(a);
-        // Complete this line
-        // Complete this line
+        System.out.println(b);
+        System.out.println(c);
     }
 }
